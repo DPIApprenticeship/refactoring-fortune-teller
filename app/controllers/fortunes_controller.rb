3e8499611d2eef04a712b670e1ba4901e74a76bd
mpuchyr <matt.puchyr@gmail.com>
2022-10-10 20:20:12 +0000

    Get fortune.html.erb and fortunes_controller working

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -2,6 +2,16 @@ class FortunesController < ApplicationController
   def horoscopes
     @sign = params.fetch(:the_sign)
 
+    all_zodiacs = Zodiac.list
+    this_zodiac = all_zodiacs.fetch(@sign.to_sym)
+    @horoscope = this_zodiac.fetch(:horoscope)
+
+    @array_of_numbers = []
+    5.times do
+      @array_of_numbers.push(rand(1..100))
+    end
+
+
     render({ :template => "fortune_templates/fortune.html.erb"})
   end
 end
